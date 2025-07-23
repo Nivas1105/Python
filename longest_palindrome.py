cf53127a9ca6ac829b5bc2f9573298b150ffefb0
Nivas <nivasmarudhavanan@gmail.com>
2025-07-22 23:46:47 -0500

    Create longest_palindrome.py

---
+++ longest_palindrome.py
@@ -0,0 +1,46 @@
+"""
+Given a string s, return the longest palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+"""
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) < 2:
+            return s
+
+        start, end = 0, 0
+
+        def expand_around_center(left: int, right: int):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return left + 1, right - 1
+
+        for i in range(len(s)):
+            # Odd length
+            l1, r1 = expand_around_center(i, i)
+            # Even length
+            l2, r2 = expand_around_center(i, i + 1)
+
+            if r1 - l1 > end - start:
+                start, end = l1, r1
+            if r2 - l2 > end - start:
+                start, end = l2, r2
+
+        return s[start:end + 1]
