887d513811daa5f690cb35a8c8abbae113f99a31
Nivas <nivasmarudhavanan@gmail.com>
2025-09-05 23:57:06 -0500

    Update collection_tuple.py

--- collection_tuple.py
+++ collection_tuple.py
@@ -90,3 +90,4 @@ from collections import namedtuple
 n, cols = int(input()), input().split()
 Student = namedtuple('Student', cols)
 print(f"{sum(int(Student(*input().split()).MARKS) for _ in range(n)) / n:.2f}")
+ 
